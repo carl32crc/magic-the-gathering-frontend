81dde52106ed4c7d648a9fc4877466fb04ddb66a
Carl Ruiz <carl.45.cr@gmail.com>
2017-09-20 00:49:41 +0200

    prepare file folders and navbar, footer, main content

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { CoreModule } from './core/core.module';
+import { ShellComponent } from './core/shell/shell.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    CoreModule,
+    HttpModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [ShellComponent]
 })
 export class AppModule { }
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ShellComponent } from './shell/shell.component';
+import { TopBarComponent } from './shell/top-bar/top-bar.component';
+import { MainContentComponent } from './shell/main-content/main-content.component';
+import { FooterComponent } from './shell/footer/footer.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    ShellComponent,
+    TopBarComponent,
+    MainContentComponent,
+    FooterComponent
+  ],
+  exports: [
+    ShellComponent
+  ]
+})
+export class CoreModule { }
