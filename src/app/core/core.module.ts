b1e56f9656536b25c3d01dc7a83db0b90a630365
Carl Ruiz <carl.45.cr@gmail.com>
2017-09-20 03:29:22 +0200

    add shared module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -5,11 +5,12 @@ import { TopBarComponent } from './shell/top-bar/top-bar.component';
 import { MainContentComponent } from './shell/main-content/main-content.component';
 import { FooterComponent } from './shell/footer/footer.component';
 import { APP_ROUTING } from './core.routes';
-
+import { SharedModule } from './../shared/shared.module';
 
 @NgModule({
   imports: [
     CommonModule,
+    SharedModule,
     APP_ROUTING
   ],
   declarations: [
