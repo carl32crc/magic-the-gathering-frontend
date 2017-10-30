bc3d702de8a925390897044f5214d09adc4f9bae
Carl Ruiz <carl.45.cr@gmail.com>
2017-10-30 01:09:04 +0100

    add config children routes in admin panel

--- src/app/admin-panel/admin-panel.routes.ts
+++ src/app/admin-panel/admin-panel.routes.ts
@@ -1,8 +1,20 @@
 import { RouterModule, Routes } from '@angular/router';
 import { AdminPanelComponent } from './admin-panel.component';
+import { ArticlesComponent } from './articles/articles.component';
+import { SliderComponent } from './slider/slider.component';
+import { UsersComponent } from './users/users.component';
 
 const ROUTES: Routes = [
-    { path: '', component: AdminPanelComponent }
+    {
+        path: '',
+        component: AdminPanelComponent,
+        children: [
+            { path: '', redirectTo: 'slider', pathMatch: 'full' },
+            { path: 'slider', component: SliderComponent },
+            { path: 'articles', component: ArticlesComponent },
+            { path: 'users', component: UsersComponent }
+        ]
+    }
 ];
 
 export const APP_ROUTING = RouterModule.forChild(ROUTES);
